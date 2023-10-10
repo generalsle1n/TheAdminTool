025f81b0ef8af553e133344235a3a1a3c612b96c
generalsle1n <niels.schuler@web.de>
2023-10-10 20:29:29 +0200

    Added new Class with Interface

---
+++ TheAdminTool/Tester/UDPTester.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheAdminTool.Tester.Interface;
+
+namespace TheAdminTool.Tester
+{
+	internal class UDPTester : ITester
+	{
+	}
+}
