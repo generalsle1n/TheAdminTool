11a04b3c0680b8290f92a3753cb0c7cbffcf24f5
generalsle1n <niels.schuler@web.de>
2023-10-10 20:26:30 +0200

    Added Root Command and Command

---
+++ TheAdminTool/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.CommandLine;
+using System.Net;
+
+RootCommand Root = new RootCommand(description: "The Admin Tool for youre easier life");
+
+Command UDPTestCommand = new Command(name: "--UDPTest", description: "Test if an remote UDP Port is reachable");
+Option<IPAddress> UDPTestIPAddressOption = new Option<IPAddress>(name: "--IP", description: "Enter the remote IP")
+{
+	IsRequired = true
+};
+Option<int> UDPTestPortOption = new Option<int>(name: "--Port", description: "Enter the port to test")
+{
+	IsRequired = true
+};
+
+UDPTestCommand.SetHandler((InputUDPTestIPAddressOption, InputUDPTestPortOption) =>
+{
+
+},UDPTestIPAddressOption, UDPTestPortOption)
