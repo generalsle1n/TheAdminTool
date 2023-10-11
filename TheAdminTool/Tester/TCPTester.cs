8241cd14be2d3927ceae4b55f590443e9221ad69
generalsle1n <niels.schuler@web.de>
2023-10-11 15:14:24 +0200

    Renamed

--- TheAdminTool/Tester/TCPTester.cs
+++ TheAdminTool/Tester/TCPTester.cs
@@ -1,19 +1,20 @@
 ﻿using System.Net;
 using System.Net.Http.Headers;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using TheAdminTool.Tester.Interface;
 using TheAdminTool.Tester.Model;
 
 namespace TheAdminTool.Tester
 {
-	public class UDPTester : ITester
+	public class TCPTester : ITester
 	{
 		public List<object> TestItems { get; set; }
 
 		public TestResult Test()
 		{
-			IPEndPoint Endpoint = new IPEndPoint((IPAddress)TestItems[0], (int)TestItems[1]);
-			UdpClient Client = new UdpClient(Endpoint);
+			IPEndPoint Endpoint = new IPEndPoint(IPAddress.Parse((string)TestItems[0]), (int)TestItems[1]);
+			
 			return TestResult.Success;
 		}
 	}
